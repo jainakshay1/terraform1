2e8cc6539595bb20b9d44c6fffa6eb59c4a149f1
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2023-01-31 11:39:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = " 2.92.0"
+    }
+  }
+}
+
+# Here we need to mention the Azure AD Application Object credentials to allow us to work with 
+# our Azure account
+
 provider "azurerm" {
-  version = "2.0"
+subscription_id = "9156aa47-68d4-4448-8f56-8412ed6e25d5"
+client_id = "27f2209f-76ec-41fb-a895-9e69a3a0ee58"
+client_secret = "lpk8Q~EVVvrlI9UFJAjfm-MErN6q1h3w-D6a2cA1"
+  tenant_id       = "7855e5f3-1a97-46df-9f2d-b5d34233f627"
+  features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "westus2"
-}
+# The resource block defines the type of resource we want to work with
+# The name and location are arguements for the resource block
 
+resource "azurerm_resource_group" "app_grp23"{
+  name="app-grp23" 
+  location="North Europe"
+}
